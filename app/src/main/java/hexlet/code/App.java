f99bfe38733ac003e5ef71ee560cd1d12710f17e
Slava <rukavanov_v@mail.ru>
2023-04-13 20:11:29 +0300

    check codeclimate

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -43,11 +43,11 @@ public class App implements Callable<Integer> {
       //  file1 = "src/test/resources/file1.yml";
       //  file2 = "src/test/resources/file2.yml";
 
-      //  file1 = "src/test/resources/file1.json";
-      //  file2 = "src/test/resources/file2.json";
+        file1 = "src/test/resources/file1.json";
+        file2 = "src/test/resources/file2.json";
 
-        file1 = "src/test/resources/fileDiff1.yml";
-        file2 = "src/test/resources/fileDiff2.yml";
+       // file1 = "src/test/resources/fileDiff1.yml";
+       // file2 = "src/test/resources/fileDiff2.yml";
 
        // file1 = "src/test/resources/fileJsonNested1.json";
        // file2 = "src/test/resources/fileJsonNested2.json";
