9a947d84321510f912fb0f6c4fad4fa09f54901f
Slava <rukavanov_v@mail.ru>
2023-04-16 09:07:53 +0300

    for hex9

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -47,7 +47,6 @@ public class Differ {
 
             return Json.getJsonOut(mapFile1, mapFile2);
         }
-
         return Stylish.getStylish(mapFile1, mapFile2).toString();
     }
 
--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -35,9 +35,7 @@ public class Stylish {
 
             }
         }
-
-        // return resultStr.append("}").append('\n');
-        return resultStr.append("}").append('\n');
-        //  return resultStr.append("}" + '"');
+      //  return resultStr.append("}").append('\n');
+        return resultStr.append("}");
     }
 }
