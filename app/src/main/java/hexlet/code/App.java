798ceff6f5abcef24a4508419cf70b550532eb2c
Slava <rukavanov_v@mail.ru>
2023-04-18 15:43:25 +0300

    try for extension 2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -47,8 +47,11 @@ public class App implements Callable<Integer> {
        // file1 = "src/test/resources/fileDiff1.yml";
        // file2 = "src/test/resources/fileDiff2.yml";
 
-        file1 = "src/test/resources/fileJsonNested1.json";
-        file2 = "src/test/resources/fileJsonNested2.json";
+    //    file1 = "src/test/resources/fileJsonNested1.json";
+     //   file2 = "src/test/resources/fileJsonNested2.json";
+
+        file1 = "src/test/resources/fileJsonTest1.json";
+        file2 = "src/test/resources/fileJsonTest2.json";
 
        // Path filepath1 = Paths.get(file1).toAbsolutePath().normalize();
        // Path filepath2 = Paths.get(file2).toAbsolutePath().normalize();
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
+import java.util.Map;
 import java.util.TreeMap;
 
 public class Parser {
@@ -12,19 +13,22 @@ public class Parser {
     public static TreeMap<String, Object> parseFile(String fileContent, String extension) throws JsonProcessingException {
 
         ObjectMapper objectMapper = null;
+        TreeMap<String, Object> map = null;
 
         if (extension.equals("yml")) {
-             objectMapper = new YAMLMapper();
+            objectMapper = new YAMLMapper();
+            map = objectMapper.readValue(fileContent, new TypeReference<>() {
+            });
         }
 
         if (extension.equals("json")) {
-             objectMapper = new ObjectMapper();
+            objectMapper = new ObjectMapper();
+            map = objectMapper.readValue(fileContent, new TypeReference<>() {
+            });
         }
-      //  ObjectMapper objectMapper = new YAMLMapper();
-
-        assert objectMapper != null;
-        TreeMap<String, Object> map = objectMapper.readValue(fileContent, new TypeReference<>() {
-        });
+        //  ObjectMapper objectMapper = new YAMLMapper();
+        //  TreeMap<String, Object> map = objectMapper.readValue(fileContent, new TypeReference<>() {
+        //   });
 
         return map;
 
