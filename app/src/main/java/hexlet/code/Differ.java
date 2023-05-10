350d5b8e4aaa7e58f2041c7ffd93fe5bac363b70
Slava <rukavanov_v@mail.ru>
2023-05-10 11:06:06 +0300

    refact Represent

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,55 +4,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.TreeMap;
 
 public class Differ {
 
-    public static List<NodeName> getDifferList(Map<String, ?> mapFile1,
-                                               Map<String, ?> mapFile2) {
 
-        List<NodeName> differList = new ArrayList<>();
-        Set<String> allKeys = new TreeSet<>();
-
-        allKeys.addAll(mapFile1.keySet());
-        allKeys.addAll(mapFile2.keySet());
-
-        for (String key : allKeys) {
-            Object valueMap1 = mapFile1.get(key);
-            Object valueMap2 = mapFile2.get(key);
-
-            if (!(mapFile1.containsKey(key)) || !(mapFile2.containsKey(key))) {
-                if (!(mapFile1.containsKey(key))) {
-                    differList.add(new NodeName(NodeStatus.ADDED, key, valueMap2, null));
-                } else {
-                    differList.add(new NodeName(NodeStatus.REMOVED, key, valueMap1, null));
-                }
-            } else {
-                if (Objects.equals(valueMap1, valueMap2)) {
-                    differList.add(new NodeName(NodeStatus.UNCHANGED, key, valueMap1, valueMap2));
-                } else {
-                    differList.add(new NodeName(NodeStatus.UPDATED, key, valueMap1, valueMap2));
-                }
-            }
-        }
-        return differList;
-    }
 
     public static String getExtension(String filePath) {
 
         return filePath.substring(filePath.indexOf(".") + 1);
     }
 
+
     public static String generate(String filepath1, String filepath2) throws Exception {
         return generate(filepath1, filepath2, "stylish");
     }
-
     public static String generate(String filepath1, String filepath2, String format) throws Exception {
 
         Map<String, Object> mapFile1 = new TreeMap<>();
@@ -63,7 +31,7 @@ public class Differ {
         mapFile2.putAll(Parser.parseFile(getFileContent(filepath2),
                 getExtension(filepath2)));
 
-        List<NodeName> differList = getDifferList(mapFile1, mapFile2);
+        List<NodeName> differList = Represent.getDifferList(mapFile1, mapFile2);
 
         return Formatter.formatter(differList, format);
 
@@ -73,5 +41,4 @@ public class Differ {
         Path path = Paths.get(filepath).toAbsolutePath().normalize();
         return Files.readString(path);
     }
-
 }
---
+++ app/src/main/java/hexlet/code/Represent.java
@@ -0,0 +1,40 @@
+package hexlet.code;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.List;
+import java.util.Map;
+
+public class Represent {
+    public static List<NodeName> getDifferList(Map<String, ?> mapFile1,
+                                               Map<String, ?> mapFile2) {
+
+        List<NodeName> differList = new ArrayList<>();
+        Set<String> allKeys = new TreeSet<>();
+
+        allKeys.addAll(mapFile1.keySet());
+        allKeys.addAll(mapFile2.keySet());
+
+        for (String key : allKeys) {
+            Object valueMap1 = mapFile1.get(key);
+            Object valueMap2 = mapFile2.get(key);
+
+            if (!(mapFile1.containsKey(key)) || !(mapFile2.containsKey(key))) {
+                if (!(mapFile1.containsKey(key))) {
+                    differList.add(new NodeName(NodeStatus.ADDED, key, valueMap2, null));
+                } else {
+                    differList.add(new NodeName(NodeStatus.REMOVED, key, valueMap1, null));
+                }
+            } else {
+                if (Objects.equals(valueMap1, valueMap2)) {
+                    differList.add(new NodeName(NodeStatus.UNCHANGED, key, valueMap1, valueMap2));
+                } else {
+                    differList.add(new NodeName(NodeStatus.UPDATED, key, valueMap1, valueMap2));
+                }
+            }
+        }
+        return differList;
+    }
+}
